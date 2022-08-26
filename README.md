765b33a61d933356c2f0e91abbc4029764490860
Utsav Gurmachhan Magar <utsavmagar88728@gmail.com>
2022-08-26 07:58:21 +0545

    added random ai

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tic-tac-toe
+
+[Animation Guide](https://jakearchibald.com/2013/animated-line-drawing-svg/)
--- js/main.js
+++ js/main.js
@@ -6,10 +6,37 @@ const NOT_MARKED = "";
 const O_MARKER = "o";
 const X_MARKER = "x";
 
-const O_MARKER_HTML = `<svg class="gameMarker" height="200px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="200px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><g><path d="M256,48C141.1,48,48,141.1,48,256s93.1,208,208,208c114.9,0,208-93.1,208-208S370.9,48,256,48z M256,446.7    c-105.1,0-190.7-85.5-190.7-190.7c0-105.1,85.5-190.7,190.7-190.7c105.1,0,190.7,85.5,190.7,190.7    C446.7,361.1,361.1,446.7,256,446.7z"/></g></g></svg>`;
-const X_MARKER_HTML = `<svg class="gameMarker" style="enable-background:new 0 0 24 24;" version="1.1" viewBox="0 0 24 24" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><style type="text/css">
-	.st0{opacity:0.2;fill:none;stroke:#000000;stroke-width:5.000000e-02;stroke-miterlimit:10;}
-</style><g id="grid_system"/><g id="_icons"><path d="M5.3,18.7C5.5,18.9,5.7,19,6,19s0.5-0.1,0.7-0.3l5.3-5.3l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3   c0.4-0.4,0.4-1,0-1.4L13.4,12l5.3-5.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L6.7,5.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4   l5.3,5.3l-5.3,5.3C4.9,17.7,4.9,18.3,5.3,18.7z"/></g></svg>`;
+const O_MARKER_HTML = `
+<svg class="gameMarker" height="200px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1"
+    viewBox="0 0 512 512" width="200px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g>
+        <g>
+            <path
+                d="M256,48C141.1,48,48,141.1,48,256s93.1,208,208,208c114.9,0,208-93.1,208-208S370.9,48,256,48z M256,446.7    c-105.1,0-190.7-85.5-190.7-190.7c0-105.1,85.5-190.7,190.7-190.7c105.1,0,190.7,85.5,190.7,190.7    C446.7,361.1,361.1,446.7,256,446.7z"
+                class="path" />
+        </g>
+    </g>
+</svg>`;
+const X_MARKER_HTML = `
+<svg class="gameMarker" style="enable-background:new 0 0 24 24;" version="1.1" viewBox="0 0 24 24" xml:space="preserve"
+    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <style type="text/css">
+        .st0 {
+            opacity: 0.2;
+            fill: none;
+            stroke: #000000;
+            stroke-width: 5.000000e-02;
+            stroke-miterlimit: 10;
+        }
+    </style>
+    <g id="grid_system" />
+    <g id="_icons">
+        <path
+            d="M5.3,18.7C5.5,18.9,5.7,19,6,19s0.5-0.1,0.7-0.3l5.3-5.3l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3   c0.4-0.4,0.4-1,0-1.4L13.4,12l5.3-5.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L6.7,5.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4   l5.3,5.3l-5.3,5.3C4.9,17.7,4.9,18.3,5.3,18.7z"
+            class="path" />
+    </g>
+</svg>`;
 
 const gameBoard = (() => {
 	let board = [
@@ -40,11 +67,15 @@ const gameBoard = (() => {
 			column_index >= 0
 		) {
 			if (board[row_index][column_index] !== NOT_MARKED) {
-				return alert("The spot has already been marked");
+				window.alert(
+					"The spot has already been marked. Please choose another location."
+				);
+				return false;
 			}
 			board[row_index][column_index] = to_mark;
+			return true;
 		} else {
-			return;
+			return false;
 		}
 	};
 	const checkBoardFilled = () => {
@@ -85,7 +116,7 @@ const computer = playerFactory("Computer", X_MARKER);
 
 const newGame = () => {
 	gameBoard.resetBoard();
-	updateBoard();
+	updateDisplayBoard();
 };
 const boardCells = document.querySelectorAll(".boardCell");
 
@@ -97,46 +128,85 @@ boardCells.forEach((cell) => {
 		console.log(
 			`Sending ${to_mark} for mark at ${row_index}, ${column_index}`
 		);
-		gameBoard.markBoard(to_mark, row_index, column_index);
-		updateBoard();
-		let win_check = gameBoard.checkWin();
-		console.log(win_check);
-
-		const overlay = document.querySelector(".overlay");
-		const winTextBlock = document.querySelector(".winTextBlock");
-		const winTitle = document.querySelector(".winTitle");
-		const winText = document.querySelector(".winText");
-		if (win_check[0] === O_MARKER || win_check[0] === X_MARKER) {
-			winTextBlock.style.display = "block";
-			overlay.style.display = "block";
-			winText.innerHTML = `${
-				player.symbol === win_check[0]
-					? player.playerName
-					: computer.playerName
-			} wins!`;
-			winTitle.innerHTML = "We have a winner!";
-		} else if (win_check[0] === "draw") {
-			winTextBlock.style.display = "block";
-			overlay.style.display = "block";
-			winText.innerHTML = "It's a draw!";
-			winTitle.innerHTML = "Draw!";
+		if (gameBoard.markBoard(to_mark, row_index, column_index)) {
+			updateDisplayBoard();
+			if (checkGameOver() > 0) {
+				return;
+			} else {
+				gameBoard.markBoard(computer.symbol, ...getComputerMove());
+				updateDisplayBoard();
+				checkGameOver();
+			}
 		}
 	});
 });
 
-const updateBoard = () => {
+const checkGameOver = () => {
+	let win_check = gameBoard.checkWin();
+	console.log(win_check);
+
+	const overlay = document.querySelector(".overlay");
+	const winTextBlock = document.querySelector(".winTextBlock");
+	const winTitle = document.querySelector(".winTitle");
+	const winText = document.querySelector(".winText");
+	if (win_check[0] === O_MARKER || win_check[0] === X_MARKER) {
+		winTextBlock.style.display = "block";
+		overlay.style.display = "block";
+		winText.innerHTML = `${
+			player.symbol === win_check[0]
+				? player.playerName
+				: computer.playerName
+		} wins!`;
+		winTitle.innerHTML = "We have a winner!";
+		return 1;
+	} else if (win_check[0] === "draw") {
+		winTextBlock.style.display = "block";
+		overlay.style.display = "block";
+		winText.innerHTML = "It's a draw!";
+		winTitle.innerHTML = "Draw!";
+		return 0.5;
+	}
+	return 0;
+};
+
+const getComputerMove = () => {
+	while (true) {
+		let row_index = Math.floor(Math.random() * 3);
+		let column_index = Math.floor(Math.random() * 3);
+		if (gameBoard.getMarkAt(row_index, column_index) === NOT_MARKED) {
+			return [row_index, column_index];
+		}
+	}
+};
+
+const resetDisplayBoard = () => {
+	const boardCells = document.querySelectorAll(".boardCell");
+	boardCells.forEach((cell) => {
+		cell.innerHTML = "";
+	});
+};
+
+const updateDisplayBoard = () => {
+	resetDisplayBoard();
 	boardCells.forEach((cell) => {
 		const row_index = cell.getAttribute("row_index");
 		const column_index = cell.getAttribute("col_index");
 		const mark = gameBoard.getMarkAt(row_index, column_index);
 		if (mark === X_MARKER) {
-			cell.innerHTML = X_MARKER_HTML;
+			cell.appendChild(createMarkerContainer(X_MARKER));
+			console.log("Adding mark X");
 		} else if (mark === O_MARKER) {
-			cell.innerHTML = O_MARKER_HTML;
-		} else {
-			cell.innerHTML = "";
+			cell.appendChild(createMarkerContainer(O_MARKER));
+			console.log("Adding mark O at " + row_index + ", " + column_index);
 		}
 	});
+};
+
+const createMarkerContainer = (marker) => {
+	let markerContainer = document.createElement("div");
+	markerContainer.innerHTML =
+		marker === X_MARKER ? X_MARKER_HTML : O_MARKER_HTML;
+	return markerContainer;
 };
 
 newGame();
